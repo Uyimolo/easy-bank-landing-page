b12ee1aa7cc5c4ddfee2453e1c0d082346fa4697
Urhoghide Justin Uyioghosa <uyijustindavid@gmail.com>
2024-03-02 20:13:40 -0800

    tablet and mobile views

--- main.js
+++ main.js
@@ -12,3 +12,40 @@ const toggleClass = (...args) => {
 menuIconsContainer.addEventListener("click", () => {
   toggleClass(openButton, closeButton, navBar);
 });
+
+
+// :root {
+//     --Mobile-Container-padding: 5rem;
+//   }
+//   header {
+//     padding: 3rem var(--Mobile-Container-padding);
+//   }
+
+//   .hero {
+//     position: relative;
+//     width: 130vw;
+//   }
+
+//   .blank {
+//     /* z-index: 1; */
+//   }
+
+//   .hero-img-container {
+//     transform: translateX(1rem);
+//     background-size: cover;
+//     background-position: bottom;
+//     background-size: 80%;
+//   }
+
+//   .hero-img {
+//     position: relative;
+//     z-index: 4;
+//     top: 3rem;
+//     /* left: -8rem; */
+//     max-width: 700px;
+//   }
+
+//   .article-tab-text {
+//     padding: 2rem;
+//   }
+
